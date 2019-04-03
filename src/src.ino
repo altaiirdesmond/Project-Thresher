39005e75d628b0d27029a9c51a4eff2f8b39231c
altaiirdesmond <altaiirdesmond@gmail.com>
2019-04-03 11:26:03 +0800

    Update src.ino

--- src/src.ino
+++ src/src.ino
@@ -1,21 +1,10 @@
 /*
-  <Project name>
+  Thrash collector
 
-  Describe what it does in layman's terms.  Refer to the components
-  attached to the various pins.
-
-  The circuit:
-   list the components attached to each input
-   list the components attached to each output
-
-  Created ...
-  By ...
-  Modified ...
-  By ...
-
+  Created Tejada
+  Modified Tejada
 */
 
-#include <SoftwareSerial.h>
 #include <Wire.h>
 #include <Servo.h>
 
@@ -23,177 +12,164 @@ Servo servo;
 
 unsigned long previousMillisTimer = 0;
 unsigned long previousMillisRPM = 0;
-unsigned long stopTime = 900000; // 15 mins
-char* recipient = "xxx";
+unsigned long stopTime = 1800000; // 30 mins
 const int relay1 = 5;
 const int trigPin1 = 9;
 const int echoPin1 = 10;
-const int trigPin2 = 11;
-const int echoPin2 = 12;
-const int interruptPin = 15;
+//const int trigPin2 = 11;
+//const int echoPin2 = 12;
+const int irPin = 13;
 
-float value = 0;
-float rev = 0;
-int rpm;
-int oldtime = 0;
-int time;
+// Ultrasonic
+long duration;
+int distance;
+
+// Tachometer
 boolean firstInit = true;
+boolean relayOff = false;
+int tryCount = 1;
 
 void setup() {
-  Serial1.begin(19200); // Listen for GSM
-  Serial2.begin(9600);  // Listen for HC-05
-  Serial.begin(9600);
+	Serial1.begin(115200); // Listen for GSM
+	Serial2.begin(9600);  // Listen for HC-05
+	Serial.begin(9600);
 
-  // Ultrasonic setup
-  pinMode(trigPin1, OUTPUT);
-  pinMode(echoPin1, INPUT);
-  pinMode(trigPin2, OUTPUT);
-  pinMode(echoPin2, INPUT);
+	// Ultrasonic setup
+	pinMode(trigPin1, OUTPUT);
+	pinMode(echoPin1, INPUT);
+	//pinMode(trigPin2, OUTPUT);
+	//pinMode(echoPin2, INPUT);
 
-  // Relay setup
-  pinMode(relay1, OUTPUT);
+	// Relay setup
+	pinMode(relay1, OUTPUT);
 
-  // Servo setup
-  servo.attach(20);
+	//
+	pinMode(irPin, INPUT);
 
-  //
-  StartGSMModule();
+	// Servo setup
+	servo.attach(20);
 
-  // Handle tachometer
-  attachInterrupt(interruptPin, isr, RISING);
+	// Default powered on
+	digitalWrite(relay1, LOW);
 }
 
 void loop() {
-  long unsigned currentMillis = millis();
-  if ((currentMillis - previousMillisTimer) >= stopTime) {
-    previousMillisTimer = currentMillis;
-    // Turn off machine after 15mins
-    digitalWrite(relay1, LOW);
-  }
+	long unsigned currentMillis = millis();
+	if ((currentMillis - previousMillisTimer) >= 10000) {
+		previousMillisTimer = currentMillis;
+		// Turn off machine after 15mins
+		digitalWrite(relay1, HIGH);
+	}
 
-  // Handle tachometer response
-  if ((currentMillis - previousMillisRPM) >= 1000) {
-    previousMillisRPM = currentMillis;
-    detachInterrupt(interruptPin);           //detaches the interrupt
-    time = millis() - oldtime;    //finds the time
-    rpm = (rev / time) * 60000;   //calculates rpm
-    oldtime = millis();           //saves the current time
-    rev = 0;
-    attachInterrupt(interruptPin, isr, RISING);
-  }
+	if (digitalRead(irPin) == LOW && !relayOff) {
+		// Reset clock
+		previousMillisRPM = currentMillis;
+	}
 
-  // Handle ultrasonic response
-  //  int duration = pulseIn(echoPin1, HIGH);
-  //  int distance = duration * 0.034 / 2;
-  //  if (distance < 5) {
-  //    // TODO
-  //  }
+	// Handle IR response
+	if ((currentMillis - previousMillisRPM) >= 2500 && !relayOff) {
+		relayOff = true;
 
-  // Check for HC-05 response
-  if (Serial2.available()) {
-    String response = Serial2.readString();
-    Serial.println(response);
-    response.trim();
-    if (response == F("POWERON")) {
-      digitalWrite(relay1, LOW);
-    }
-    else if (response == F("POWEROFF")) {
-      digitalWrite(relay1, HIGH);
-    }
-  }
+		// Turn off relay immediately
+		digitalWrite(relay1, HIGH);
+		
+		// Increment tries
+		if (tryCount++ < 2) {
+			SendTo("9366092093", "WARNING: OBJECT STUCKED\n\nTry again? Reply yes to retry or no to stop");
+			
+			// Set to recieving mode.
+			Serial.print(F("Setting to receiving mode..."));
+			while (1) {
+				Serial1.println(F("AT+CNMI=2,2,0,0,0"));
 
-  // Check for GSM900 response
-  if (Serial1.available()) {
-    Serial.print(Serial1.readString());
-  }
+				while (!Serial1.available());
+				if (Serial1.find("OK")) {
+					Serial.println(F("OK"));
+					break;
+				}
+			}
+
+			// And wait reply of user.
+			while (1) {
+				while (!Serial1.available());
+				if (Serial1.find("yes")) {
+					Serial.println(F("yes"));
+					digitalWrite(relay1, HIGH);
+
+          // Start checking for rotation again
+					relayOff = false;
+         
+					break;
+				}
+				else if (Serial1.find("no")) {
+					Serial.println(F("no"));
+					break;
+				}
+			}
+		}
+		else { 
+			// 
+			digitalWrite(relay1, HIGH);
+			SendTo("9366092093", "WARNING: OBJECT STUCKED");
+			Serial.println("OFF");
+		}
+	}
+
+	// Check for HC-05 response
+	if (Serial2.available()) {
+		String response = Serial2.readString();
+		Serial.println(response);
+		response.trim();
+		if (response == F("POWERON")) {
+			digitalWrite(relay1, LOW);
+
+      // Start checking for rotation again
+      relayOff = false;
+		}
+		else if (response == F("POWEROFF")) {
+			digitalWrite(relay1, HIGH);
+		}
+	}
+
+	// Ultrasonic
+	CheckDistance();
 }
 
-void StartGSMModule() {
-  Serial.println(F("Setting up SIM900"));
+void SendTo(char *number, String message) {
+	Serial1.print(F("Sending to"));
+	Serial1.println(number);
 
-  Serial.print(F("Checking device..."));
-  while (1) {
-    Serial1.println(F("AT"));
+	*(number + 10) = '\0';
 
-    while (!Serial1.available());
-    if (Serial1.find("OK")) {
-      Serial.println(F("OK"));
-      break;
-    }
-  }
+	Serial1.print(F("AT+CMGS =\""));
+	Serial1.print(number);
+	Serial1.print(F("\"\r\n"));
+	delay(1000);
+	Serial1.println(message);
+	delay(1000);
+	Serial1.println((char)26);
+	delay(3000);
 
-  Serial.print(F("Setting to text mode..."));
-  while (1) {
-    Serial1.println(F("AT+CMGF=1"));
-
-    while (!Serial1.available());
-    if (Serial1.find("OK")) {
-      Serial.println(F("OK"));
-      break;
-    }
-  }
-
-  Serial.print(F("Setting to receiving mode..."));
-  while (1) {
-    Serial1.println(F("AT+CNMI=2,2,0,0,0"));
-
-    while (!Serial1.available());
-    if (Serial1.find("OK")) {
-      Serial.println(F("OK"));
-      break;
-    }
-  }
-
-  Serial.print(F("Finding network..."));
-  while (1) {
-    Serial1.println(F("AT+CPIN?"));
-
-    while (!Serial1.available());
-    if (Serial1.find("+CPIN: READY")) {
-      Serial.println(F("Network found"));
-      break;
-    }
-  }
-
-  Serial.print(F("Checking signal..."));
-  while (1) {
-    Serial1.println(F("AT+CSQ"));
-
-    while (!Serial1.available());
-    if (Serial1.find("+CSQ")) {
-      Serial.println(Serial1.readString());
-      break;
-    }
-  }
-
-  Serial1.println(F("AT+CMGD = 1,4\r"));
-
-  Serial.println(F("GSM ready to be used"));
+	Serial.println("Sent");
 }
 
-void isr() { //interrupt service routine
-  rev++;
-}
+void CheckDistance() {
+	// Clears the trigPin
+	digitalWrite(trigPin1, LOW);
+	delayMicroseconds(2);
+	// Sets the trigPin on HIGH state for 10 micro seconds
+	digitalWrite(trigPin1, HIGH);
+	delayMicroseconds(10);
+	digitalWrite(trigPin1, LOW);
+	// Reads the echoPin, returns the sound wave travel time in microseconds
+	duration = pulseIn(echoPin1, HIGH);
+	// Calculating the distance
+	distance = duration * 0.034 / 2;
+	// Prints the distance on the Serial Monitor
+	Serial.print("Distance: ");
+	Serial.println(distance);
 
-bool proximity(long &dist) {
-  // TODO
-}
-
-void NotifyNumber(String number, String message) {
-  char num[9];
-  char* numPtr = num;
-
-  number.toCharArray(num, 11);
-
-  num[10] = '\0';
-
-  Serial.println("-------------------");
-  Serial1.print(F("AT+CMGS =\""));
-  Serial1.print(numPtr);
-  Serial1.print(F("\"\r\n"));
-  delay(1000);
-  Serial1.println(message);
-  delay(1000);
-  Serial1.println((char)26);
-  delay(1000);
+	if (distance <= 4) {
+		SendTo("9366092093", "WARNING BIN FULL");
+	}
 }
