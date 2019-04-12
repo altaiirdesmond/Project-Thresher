62c713458d11e4a7e6334af9876b542199f58212
altaiirdesmond <altaiirdesmond@gmail.com>
2019-04-12 20:07:00 +0800

    Update serialEvent added and some refactors

--- src/src.ino
+++ src/src.ino
@@ -12,269 +12,242 @@ Servo servo;
 
 unsigned long previousMillisTimer = 0;
 unsigned long previousMillisRPM = 0;
-const unsigned long seconds = 1000;
-const unsigned long hour = 3600 * seconds;
-unsigned long activeTime = 1 * hour;
-unsigned long inactiveTime = 2 * hour;
+const unsigned long SECONDS = 1000;
+const unsigned long HOUR = 3600 * SECONDS;
+unsigned long activeTime = 1 * HOUR;
+unsigned long inactiveTime = 2 * HOUR;
 
 // Pins
-const int relay = 5;
-const int trigPin1 = 9;
-const int echoPin1 = 10;
-const int trigPin2 = 11;
-const int echoPin2 = 12;
-const int irPin = 13;
+const int RELAY_PIN = 5;
+const int TRIG_PIN_1 = 9;
+const int ECHO_PIN_1 = 10;
+const int TRIG_PIN_2 = 11;
+const int ECHO_PIN_2 = 12;
+const int IR_PIN = 13;
 
 // Ultrasonic
-long duration;
-int distance;
-boolean ultrasonic_0 = true;
+boolean ultrasonic_1_init = true;
 
-// Tachometer
+// IR
 boolean firstInit = true;
 boolean relayOff = false;
-int tryCount = 1;
+boolean irEvaluate = false;
+int isObstacle = HIGH;
 
 // Signals for machine timer of inactivity or activity
 boolean machineActive = true;
 boolean permaOff = false;
 
 void setup() {
-  // Listen for GSM
-  Serial1.begin(115200);  
-  // Listen for HC-05
-  Serial2.begin(9600);
-  // Debugging purpose    
-  Serial.begin(9600);
-  // Ultrasonic setup
+	// Listen for GSM
+	Serial1.begin(115200);
+	// Listen for HC-05
+	Serial2.begin(9600);
+	// Debugging purpose
+	Serial.begin(9600);
 
-  Serial.println("Initializing.");
-  
-  pinMode(trigPin1, OUTPUT);
-  pinMode(echoPin1, INPUT);
-  pinMode(trigPin2, OUTPUT);
-  pinMode(echoPin2, INPUT);
+	// Ultrasonic setup
+	pinMode(TRIG_PIN_1, OUTPUT);
+	pinMode(ECHO_PIN_1, INPUT);
+	pinMode(TRIG_PIN_2, OUTPUT);
+	pinMode(ECHO_PIN_2, INPUT);
 
-  // Relay setup
-  pinMode(relay, OUTPUT);
+	// Relay setup
+	pinMode(RELAY_PIN, OUTPUT);
 
-  // IR setup
-  pinMode(irPin, INPUT);
+	// IR setup
+	pinMode(IR_PIN, INPUT);
 
-  // Servo setup
-  servo.attach(7);
+	// Servo setup
+	servo.attach(7);
 
-  // Default powered on
-  digitalWrite(relay, LOW);
+	// Default powered on
+	digitalWrite(RELAY_PIN, LOW);
 
-  Serial.println("Done.");
+	// Defaults to first bin
+	servo.write(135);
+
+	Serial.println(F("Started."));
 }
 
 void loop() {
-  long unsigned currentMillis = millis();
+	long unsigned currentMillis = millis();
 
-  // Will watch during active time
-  if ((currentMillis - previousMillisTimer) >= activeTime && machineActive) {
-    previousMillisTimer = currentMillis;
-    // Turn off machine after 1 hour
-    digitalWrite(relay, HIGH);
+	// Will watch during active time
+	if ((currentMillis - previousMillisTimer) >= activeTime && machineActive) {
+		previousMillisTimer = currentMillis;
+		// Turn off machine after 1 HOUR
+		digitalWrite(RELAY_PIN, HIGH);
 
-    // Will start cycle of inactivity
-    machineActive = false;
+		// Will start cycle of inactivity
+		machineActive = false;
 
-    // Prevent IR detection
-    relayOff = true;
-  }
+		// Prevent IR detection
+		relayOff = true;
+	}
 
-  // Will watch during inactive time
-  if ((currentMillis - previousMillisTimer) >= inactiveTime && !machineActive) {
-    previousMillisTimer = currentMillis;
-    // Turn on machine after 2 hours
-    digitalWrite(relay, LOW);
+	// Will watch during inactive time
+	if ((currentMillis - previousMillisTimer) >= inactiveTime && !machineActive) {
+		previousMillisTimer = currentMillis;
+		// Turn on machine after 2 hours
+		digitalWrite(RELAY_PIN, LOW);
 
-    // Will start cycle of activity
-    machineActive = true;
+		// Will start cycle of activity
+		machineActive = true;
 
-    // Restore IR detection
-    relayOff = false;
-  }
+		// Restore IR detection
+		relayOff = false;
+	}
 
-  // Neglects whether machine is inactive or active. Machine will be fully disabled
-  if(permaOff) {
-    // Force timer to be 0
-    currentMillis = 0;
-  }
+	// Neglects whether machine is inactive or active. Machine will be fully
+	// disabled
+	if (permaOff) {
+		// Force timer to be 0
+		currentMillis = 0;
+	}
+	
+	isObstacle = digitalRead(IR_PIN);
+	// Whenever IR pin has detected something
+	if (isObstacle == LOW && !relayOff) {
+		// Reset clock
+		previousMillisRPM = currentMillis;
+	}
 
-  // Whenever IR pin has detected something
-  if (digitalRead(irPin) == LOW && !relayOff) {
-    // Reset clock
-    previousMillisRPM = currentMillis;
-    Serial.println("detected");
-  }
+	// Handle IR response
+	if ((currentMillis - previousMillisRPM) >= 5000 && (currentMillis - previousMillisRPM) <= 5500 && !relayOff) {
+		// Relay off
+		digitalWrite(RELAY_PIN, HIGH);
 
-  // Handle IR response
-  if ((currentMillis - previousMillisRPM) >= 2500 && !relayOff) {
-    relayOff = true;
+		// Perma off
+		permaOff = true;
 
-    // Turn off relay immediately
-    digitalWrite(relay, HIGH);
+		// Prevent IR detection
+		relayOff = true;
+	}
 
-    // Increment tries
-    if (tryCount++ < 2) {
-      SendTo("9366092093",
-             "WARNING: OBJECT STUCKED\n\nTry again? Reply yes to retry or no "
-             "to stop");
-
-      // Set to recieving mode.
-      Serial.print(F("Setting to receiving mode..."));
-      while (1) {
-        Serial1.println(F("AT+CNMI=2,2,0,0,0"));
-
-        while (!Serial1.available())
-          ;
-        if (Serial1.find("OK")) {
-          Serial.println(F("OK"));
-          break;
-        }
-      }
-
-      // And wait reply of user.
-      while (1) {
-        while (!Serial1.available())
-          ;
-        if (Serial1.find("yes")) {
-          Serial.println(F("yes"));
-          digitalWrite(relay, HIGH);
-
-          // Start checking for rotation again
-          relayOff = false;
-
-          break;
-        } else if (Serial1.find("no")) {
-          Serial.println(F("no"));
-          break;
-        }
-      }
-    } else {
-      digitalWrite(relay, HIGH);
-      SendTo("9162277397", "WARNING: OBJECT STUCKED");
-      Serial.println("OFF");
-    }
-  }
-
-  // Check for HC-05/Bluetooth incoming message
-  if (Serial2.available()) {
-    String response = Serial2.readString();
-    Serial.println(response);
-    response.trim();
-    if (response == F("POWERON")) {
-      // Relay on
-      digitalWrite(relay, LOW);
-
-      // Remove permaOff
-      permaOff = false;
-
-      // Signal to start the 1 Hour of activity
-      machineActive = true;
-
-      // Start checking for rotation again
-      relayOff = false;
-
-      // After bins are full wait for App to signal an ON then switch to the first bin
-      servo.write(45);
-    } else if (response == F("POWEROFF")) {
-      // Relay off
-      digitalWrite(relay, HIGH);
-
-      // Perma off
-      permaOff = true;
-
-      // Prevent IR detection
-      relayOff = true;
-    }
-  }
-
-  // Servo will only work if machine is in active state
-  if (ultrasonic_0 && machineActive) {
-    // First ultrasonic
-    firstUltrasonic();
-  } else if(!ultrasonic_0 && machineActive) {
-    // Second ultrasonic
-    secondUltrasonic();
-  }
+	// Servo will only work if machine is in active state
+	if (ultrasonic_1_init && machineActive) {
+		// First ultrasonic
+		firstUltrasonic();
+	}
+	else if (!ultrasonic_1_init && machineActive) {
+		// Second ultrasonic
+		secondUltrasonic();
+	}
 }
 
 void SendTo(char *number, String message) {
-  Serial1.print(F("Sending to"));
-  Serial1.println(number);
+	Serial1.print(F("Sending to"));
+	Serial1.println(number);
 
-  *(number + 10) = '\0';
+	*(number + 10) = '\0';
 
-  Serial1.print(F("AT+CMGS =\""));
-  Serial1.print(number);
-  Serial1.print(F("\"\r\n"));
-  delay(1000);
-  Serial1.println(message);
-  delay(1000);
-  Serial1.println((char)26);
-  delay(3000);
+	Serial1.print(F("AT+CMGS =\""));
+	Serial1.print(number);
+	Serial1.print(F("\"\r\n"));
+	delay(1000);
+	Serial1.println(message);
+	delay(1000);
+	Serial1.println((char)26);
+	delay(3000);
 
-  Serial.println("Sent");
+	Serial.println(F("Sent"));
+}
+
+// Check for HC-05/Bluetooth incoming message
+void serialEvent2() {
+	if (Serial2.available()) {
+		char data[20];
+		strcpy(data, Serial2.readString().c_str());
+		if (strstr(data, "POWERON") != nullptr) {
+			Serial.println("STATE ON");
+
+			// Relay on
+			digitalWrite(RELAY_PIN, LOW);
+
+			// Remove permaOff
+			permaOff = false;
+
+			// Signal to start the 1 Hour of activity
+			machineActive = true;
+
+			// Start checking for rotation again
+			relayOff = false;
+
+			// After bins are full wait for App to signal an ON then switch to the
+			// first bin
+			servo.write(135);
+		}
+		else if (strstr(data, "POWEROFF") != nullptr) {
+			Serial.println("STATE OFF");
+
+			// Relay off
+			digitalWrite(RELAY_PIN, HIGH);
+
+			// Perma off
+			permaOff = true;
+
+			// Prevent IR detection
+			relayOff = true;
+		}
+	}
 }
 
 void firstUltrasonic() {
-  // Clears the trigPin
-  digitalWrite(trigPin1, LOW);
-  delayMicroseconds(2);
-  // Sets the trigPin on HIGH state for 10 micro seconds
-  digitalWrite(trigPin1, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin1, LOW);
-  // Reads the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(echoPin1, HIGH);
-  // Calculating the distance
-  distance = duration * 0.034 / 2;
-  // Prints the distance on the Serial Monitor
-  Serial.print("Distance: ");
-  Serial.println(distance);
+	// Clears the trigPin
+	digitalWrite(TRIG_PIN_1, LOW);
+	delayMicroseconds(2);
+	// Sets the trigPin on HIGH state for 10 micro SECONDS
+	digitalWrite(TRIG_PIN_1, HIGH);
+	delayMicroseconds(10);
+	digitalWrite(TRIG_PIN_1, LOW);
+	// Reads the echoPin, returns the sound wave travel time in microseconds
+	long duration = pulseIn(ECHO_PIN_1, HIGH);
+	// Calculating the distance
+	float distance = duration * 0.034 / 2;
+	// Prints the distance on the Serial Monitor
+	Serial.print(F("Distance: "));
+	Serial.println(distance);
 
-  if (distance <= 24) {
-    // Switch to second ultrasonic
-    ultrasonic_0 = false;
+	if (distance <= 21) {
+		// Switch to second ultrasonic
+		ultrasonic_1_init = false;
 
-    // Reset this values for second ultrasonic
-    duration = 0;
-    distance = 0;
+		// Reset this values for second ultrasonic
+		duration = 0;
+		distance = 0;
 
-    // Change to second bin
-    servo.write(135);
-  }
+		// Change to second bin
+		servo.write(45);
+	}
 }
 
 void secondUltrasonic() {
-  digitalWrite(trigPin2, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin2, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin2, LOW);
-  duration = pulseIn(echoPin2, HIGH);
-  distance = duration * 0.034 / 2;
-  Serial.print("Distance: ");
-  Serial.println(distance);
+	digitalWrite(TRIG_PIN_2, LOW);
+	delayMicroseconds(2);
+	digitalWrite(TRIG_PIN_2, HIGH);
+	delayMicroseconds(10);
+	digitalWrite(TRIG_PIN_2, LOW);
+	long duration = pulseIn(ECHO_PIN_2, HIGH);
+	float distance = duration * 0.034 / 2;
+	Serial.print(F("Distance: "));
+	Serial.println(distance);
 
-  if (distance <= 24) {
-    // Switch to first ultrasonic
-    ultrasonic_0 = true;
+	if (distance <= 21) {
+		servo.write(135);
 
-    // Turn relay off
-    digitalWrite(relay, HIGH);
-    relayOff = true;
+		// Switch to first ultrasonic
+		ultrasonic_1_init = true;
 
-    // Disable all activities until HC-05 sends ON
-    permaOff = true;
-    
-    Serial.print("Notifying recipient...");
-    SendTo("9162277397", "WARNING: BINS ARE FULL");
-    Serial.println("OK");
-  }
+		// Turn RELAY_PIN off
+		digitalWrite(RELAY_PIN, HIGH);
+		relayOff = true;
+
+		// Disable all activities until HC-05 sends ON
+		permaOff = true;
+
+		Serial.print(F("Notifying recipient..."));
+		SendTo("9162277397", "WARNING: BINS ARE FULL");
+		Serial.println(F("OK"));
+	}
 }
